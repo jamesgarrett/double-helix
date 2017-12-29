6eca504b9165972e1f214b95c343cf9e77d7d500
James Garrett <jgsibinga@gmail.com>
2017-12-29 16:14:27 -0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,56 @@
+var xspacing = 16;    // Distance between each horizontal location
+var w;                // Width of entire wave
+var theta = 0.0;      // Start angle at 0
+var amplitude = 75.0; // Height of wave
+var period = 50.0;   // How many pixels before the wave repeats
+var dx;               // Value for incrementing x
+var yvalues;  // Using an array to store height values for the wave
+var cvalues;
+
+function setup() {
+  var wwidth = window.innerWidth;
+  var wheight = window.innerHeight;
+  createCanvas(wwidth, wheight);
+  w = width+16;
+  dx = (TWO_PI / period) * xspacing;
+  yvalues = new Array(floor(w/xspacing));
+  cvalues = new Array(floor(1000/xspacing));
+}
+
+function draw() {
+  calcHue(HSB);
+  calcWave();
+  renderWave();
+}
+
+function calcHue(){
+  background(cvalues);
+}
+
+function calcWave() {
+  // Increment theta (try different values for 
+  // 'angular velocity' here)
+  theta += 0.02;
+
+  // For every x value, calculate a y value with sine function
+  var x = theta;
+  for (var i = 0; i < yvalues.length; i++) {
+    yvalues[i] = sin(x)*amplitude;
+    x+=dx;
+  }
+  for (var j = 0; j < cvalues.length; j++){
+    var y = 1;
+    y += 35;
+    cvalues[j] = y;
+  }
+    
+}
+
+function renderWave() {
+  noStroke();
+  fill(255);
+  // A simple way to draw the wave with an ellipse at each location
+  for (var x = 0; x < yvalues.length; x++) {
+    ellipse(x*xspacing, height/2+yvalues[x], 16, 16);
+  }
+}
